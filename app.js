b5cab5f7c7509ee3cd407f7776e0ba244351ef58
kareemah <ajimobikareemah@gmail.com>
2023-02-13 13:55:07 +0700

    connect to db

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const app = express();
 const port = 3000;
 const tasks = require('./routes/tasks');
 
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
 app.use(express.json());
 app.use('/api/v1/tasks', tasks);
 
-// app.get('/hello', (req, res) => {
-//   res.send('hello');
-// });
-
-app.listen(port, console.log(`Server is listening on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB(
+      'mongodb+srv://Kareemah:Ajimobi20_$$@taskmanager.epyzcdb.mongodb.net/task-manager?retryWrites=true&w=majority'
+    );
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
